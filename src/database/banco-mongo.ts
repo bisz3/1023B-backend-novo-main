50985c28a60b90423318e54fff033f63dd171c74
bisz3 <lorrainy.peralta@estudante.ifms.edu.br>
2025-10-20 08:32:49 -0300

    duablio

--- src/database/banco-mongo.ts
+++ src/database/banco-mongo.ts
@@ -1,6 +1,10 @@
 import { MongoClient } from 'mongodb'
 
-const client = new MongoClient(process.env.MONGOURI!)
+const client = new MongoClient(process.env.MONGOURI!, {
+  tls: true,
+  tlsAllowInvalidCertificates: true,
+  tlsAllowInvalidHostnames: true,
+})
 await client.connect()
 const db = client.db(process.env.MONGODB!)
 
